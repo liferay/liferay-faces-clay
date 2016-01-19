c67014a4b7d1b6d71bd327a2139e23c45be909a8
Kyle Stiemann <stiemannkj1@gmail.com>
2016-01-19 15:56:25 -0500

    FACES-2544 Change API of the ResourceDependencyVerifier.isResourceDependencySatisfied(UIComponent) to ResourceVerifier.isDependencySatisfied(FacesContext, UIComponent)

---
+++ metal/src/main/java/com/liferay/faces/metal/application/internal/ResourceVerifierMetalImpl.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.metal.application.internal;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import com.liferay.faces.util.application.ResourceUtil;
+import com.liferay.faces.util.application.ResourceVerifier;
+import com.liferay.faces.util.application.ResourceVerifierWrapper;
+import com.liferay.faces.util.product.ProductConstants;
+import com.liferay.faces.util.product.ProductMap;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class ResourceVerifierMetalImpl extends ResourceVerifierWrapper {
+
+	// Private Constants
+	private static final boolean LIFERAY_PORTAL_DETECTED = ProductMap.getInstance().get(ProductConstants.LIFERAY_PORTAL)
+		.isDetected();
+	private static final String BOOTSTRAP_CSS_RESOURCE_ID = ResourceUtil.getResourceId("liferay-faces-metal-reslib",
+			"css/bootstrap.min.css");
+
+	// Private Members
+	private ResourceVerifier wrappedResourceVerifier;
+
+	public ResourceVerifierMetalImpl(ResourceVerifier wrappedResourceVerifier) {
+		this.wrappedResourceVerifier = wrappedResourceVerifier;
+	}
+
+	@Override
+	public boolean isDependencySatisfied(FacesContext facesContext, UIComponent componentResource) {
+
+		boolean dependencySatisfied;
+
+		if (LIFERAY_PORTAL_DETECTED &&
+				BOOTSTRAP_CSS_RESOURCE_ID.equals(ResourceUtil.getResourceId(componentResource))) {
+			dependencySatisfied = true;
+		}
+		else {
+			dependencySatisfied = super.isDependencySatisfied(facesContext, componentResource);
+		}
+
+		return dependencySatisfied;
+	}
+
+	@Override
+	public ResourceVerifier getWrapped() {
+		return wrappedResourceVerifier;
+	}
+}
